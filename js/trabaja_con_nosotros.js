25dae591b60c3c21fc1afb86b9b79968b8beebaa
bedoya199712 <carlosa.bedoyar@uqvirtual.edu.co>
2023-08-23 15:14:18 -0500

    Se creo la funcionalidad de enviar correo en la seccion de conactanos, se diseño la pagina web trabaja con nosotros y se agrego la funcionalidad de enviar correo

--- js/trabaja_con_nosotros.js
+++ js/trabaja_con_nosotros.js
@@ -1,34 +1,31 @@
 // ENVIAR CORREO 
-// CAPTURO EL ELEMENTO BOTON DEL FORM
-const botonForm = document.getElementById('botonForm');
+// CAPTURO EL ELEMENTO BOTON DEL FORM TRABAJA CON NOSOTROS
+const botonFormTrabajaConNosotros = document.getElementById('botonFormTrabajaConNosotros');
 // EVENTO QUE OCURRE CUANDO SE LE DA CLICK AL BOTON
-botonForm.addEventListener('click', function () {
+botonFormTrabajaConNosotros.addEventListener('click', function () {
     //CAPTURA DE ELEMENTOS DEL FORMULARIO
     const nombre = document.getElementById('nombre');
     const email = document.getElementById('email');
     const celular = document.getElementById('celular');
-    const asunto = document.getElementById('asunto');
+    const cargo = document.getElementById('contact_cargo');
     const validacionEmail = document.getElementById('validacionEmail');
     //VALIDACION DEL CAMPO EMAIL
     if (email.value == '') {
         validacionEmail.innerHTML = "<p><strong>El campo email es requerido </strong></p>"
     } else {
         //CREO EL BODY DEL CORREO
-        const AsuntoMensaje = 'Solicitud de información';
-        const bodyMensaje = `Nombre: ${nombre.value} %0AEmail: ${email.value} %0ACelular: ${celular.value} %0AAsunto: ${asunto.value}`;
+        const AsuntoMensaje = 'Candidato de empleo';
+        const bodyMensaje = `Nombre: ${nombre.value} %0AEmail: ${email.value} 
+        %0ACelular: ${celular.value} %0ACargo al que aspiro: ${cargo.options[cargo.selectedIndex].value}`;
         //PRIMER PARAMETRO CORREO AL QUE SERA ENVIADO, SEGUNDO EL ASUNTO DEL CORREO(SUBJECT), TERCERO BODY DEL CORREO(BODY)
         window.location.href = `mailto:carlos_bedoya97@hotmail.com?subject=${AsuntoMensaje}&body=${bodyMensaje}`;
         //LIMPIO TODOS LOS CAMPOS
         nombre.value = '';
         email.value = '';
         celular.value = '';
-        asunto.value = '';
+        cargo.value = 0;
         //LIMPIO LA VALIDACION DEL EMAIL
         validacionEmail.innerText='';
 
     }
 })
-
-
-
-
